d9686d189003036d79f078f04582673eabc806a5
dianestephani <diane.stephani@gmail.com>
2020-08-10 00:11:05 -0700

    constructor function made

--- index.js
+++ index.js
@@ -4,8 +4,18 @@
 const express = require('express');
 const app = express();
 
-const minimist = require('minimist');
+// const minimist = require('minimist');
+// const args = minimist(process.argv.slice(2));
 
-console.log('This is my working server. It works super well because I am so smart.');
+// const request = require('./input.js/'); This line is the problem
+
+// console.log(noteObject); This line is also problematic
+
 
 app.listen(3000);
+
+//Requires library files (input, notes).
+
+//Instantiates an instance of an "Input" parser module, which parses the command line input and returns the command and data.
+
+//Passes the command to the Notes library, which executes the command.
